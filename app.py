c7f52693c1e98cf68755e7c45783006236484a84
upendri <awekshikaupendri3129@gmail.com>
2026-04-24 10:07:54 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import flask
+
+app = flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, DevOps world V1"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
